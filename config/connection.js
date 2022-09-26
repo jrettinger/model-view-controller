4210b910bdf95860ee2a21d1ad60f17328f13920
unknown <jason.rettinger@gmail.com>
2022-09-26 14:51:52 -0400

    created connection.js

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require("sequelize");
+
+// SETUP THE DB CONFIG
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3301,
+  }
+);
+
+sequelize.sync();
+
+// AUTHENTICATE THE CONNECTION
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+
+module.exports = sequelize;
