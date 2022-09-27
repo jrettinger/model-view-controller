78d4a5f906bf29ab7eef64145beefa88af7b4652
unknown <jason.rettinger@gmail.com>
2022-09-27 15:36:24 -0400

    created seed.js

---
+++ seed.js
@@ -0,0 +1,20 @@
+const seedBlogs = require("./seeds/blog-seeds");
+const seedUsers = require("./seeds/user-seeds");
+const seedComments = require("./seeds/comment-seeds");
+
+const sequelize = require("./db/connection");
+const seedAll = async () => {
+  // await sequelize.sync({ force: true });
+  // console.log("\n---Database Synced---\n");
+
+  await seedUsers();
+  console.log("\n---User Seeded---\n");
+
+  await seedBlogs();
+  console.log("\n---Blog Seeded---\n");
+
+  await seedComments();
+  console.log("\n---Comment Seeded---\n");
+};
+
+seedAll();
